718ca0dbd73976bf4b73722fb8f88e0ad6b10291
Tenywa <samtenua@gmail.com>
2023-11-30 21:18:02 +0300

    Adding another file to the Git project

---
+++ FirstFile.java
@@ -0,0 +1,11 @@
+//import java.util.*;
+
+public class FirstFile {
+	public static void main(String[] args) {
+		//Scanner input = new Scanner("System.in");
+		System.out.println("How are you");
+		//String x = input.nextLine();
+		//System.out.println(x);
+		
+	} 
+}
